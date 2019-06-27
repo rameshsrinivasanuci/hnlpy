fed9808bcf1992edafcb1f191e1b6bfb5f80c70f
mtisby <mariel@rchair.ss.uci.edu>
2019-06-27 15:39:11 -0700

    convert to CSV
    HDDM Testing Script

---
+++ CSVConvFinal.py
@@ -0,0 +1,95 @@
+# Mariel Tisby
+# HNL Project CSV Conversion Multiple Subject Data Files
+# 6.27.19
+
+import h5py 
+import numpy as np 
+import os
+import csv
+from itertools import zip_longest 
+
+# get // check current directory
+cwd = os.getcwd()
+print('Current Directory: ', cwd)
+
+# change directory
+new_dir = '/data/pdmattention/task3' #type file path for new directory with data
+os.chdir(new_dir)
+print('Current Directory: ', os.getcwd())
+
+# get the files in the target directory
+# file names are stored in variable sub_files 
+# subject ids are stored in variable sub_IDs
+data_files = os.listdir(cwd)
+
+sub_files = []
+sub_IDs = []
+
+for cur_file in data_files: # iterates through every file in directory
+	if os.path.isfile(cur_file) == True: # checks to see if file is a file
+		if cur_file[16:27] == 'expinfo.mat': # specifies the file type
+			sub_files.append(cur_file)
+			sub_IDs.append(cur_file[0:4])
+
+# goes through every subject and extracts data 
+for sub in range(len(sub_files)):
+	
+	current_sub = sub_files[sub]
+	print('subject:',current_sub)
+	arrays = {}
+
+	# opens file and extracts all data using numpy 
+	f = h5py.File(current_sub)
+	for k, v in f.items():
+		arrays[k] = np.array(v)
+
+	f.close()
+
+	rt_list = []
+	correct_list = []
+	condition_list = []
+	sub_list = []
+
+	rts = arrays['rt']
+	conditions = arrays['condition']
+	corrects = arrays['correct']
+
+	# extracts all data and puts data in a list
+	for ii in range(len(rts)):
+		rt_list.append(rts[ii][0])
+		correct_list.append(corrects[ii][0])
+		condition_list.append(conditions[ii][0])
+		sub_list.append(sub_IDs[sub])
+
+	data = [sub_list, condition_list, rt_list, correct_list]
+
+	# zips lists together, making it easier to write to csv file
+	export_data = zip_longest(*data, fillvalue='') # zip_longest makes an iterator 
+		                                       # that aggregates elements from
+		                                       # each of the iterables; if uneven
+		                                       # missing values are filled-in
+		                                       # with fillvalue
+
+	# change to directory you want csv file to be saved in 
+	os.chdir(cwd)
+	
+	if sub == 0:
+		with open('TestData.csv', 'w', encoding = 'ISO-8859_1', newline='') as csvFile:
+			wr = csv.writer(csvFile) # returns a writer object responsible for
+						 # converting
+		                             # the user's data into strings; we can use this
+		                             # object to manipulate excel files
+
+			wr.writerow(('subj_idx', 'condition', 'rt', 'correct')) # writes the  											# headers
+			wr.writerows(export_data) # writes the data
+
+        		
+	else:
+		with open('TestData.csv','a', encoding = 'ISO-8859_1', newline='') as csvFile:
+			wr = csv.writer(csvFile)
+			wr.writerows(export_data)
+        		
+		
+	os.chdir(new_dir) # change directory back to where data is
+
+	
---
+++ HDDMPrac.py
@@ -0,0 +1,121 @@
+# Mariel Tisby
+# 5/21/19
+
+# HNL HDDM Project
+
+''' in Anaconda Prompt Type
+
+>>> activate snakes # activates environment which contains Python 3.5
+>>> cd Documents\Python_Projects
+>>> python HDDMPrac.py
+
+'''
+import time # for readers use to pause program
+
+import pandas as pd
+import matplotlib.pyplot as plt # matplotlib is a 2D plotting library
+                                # matplotlib.pyplot is a collection of
+                                # comannd style functions that make
+                                # matplotlib work like MATLAB
+# aliasing modules
+# import [module] as [another name]
+
+print('working...')
+print(' ')
+
+import hddm
+print('HDDM VER:', hddm.__version__) # check
+
+def main():
+
+    data = hddm.load_csv('TestData.csv')
+    data.head(10) # returns first n rows for the object based on position
+
+    # data = hddm.utils.flip_errors(data) # flips sign for lower boundary
+                                        # response --> what are lower
+                                        # boundaries
+
+    fig = plt.figure() # creates an empty figure
+    ax = fig.add_subplot(111, xlabel = 'RT', ylabel='count', title='RT distributions')
+
+    for i, subj_data in data.groupby('subj_idx'):
+
+        # data.groupby splits that data into the groups based on some criterion
+        # grouped = obj.groupby(key)
+        # grouped = obj.groupby(key, axis = 1)
+        # grouped = obj.groupby([key1, key2])
+
+        # data.groupby('subj_idx') groups data horizontally by
+        # the column subj_idx
+        
+        
+       for i, subj_data in data.groupby('subj_idx'):
+            subj_data.rt.hist(bins=20, histtype='step', ax=ax)
+
+    plt.savefig('hddm_demo_fig_00.pdf')
+    plt.show()
+
+    # time.sleep(5)
+    print('\n')
+    print('Done!')
+
+    next_step(data)
+
+def next_step(data):
+    print('working... \n')
+    m = hddm.HDDM(data)
+
+    # find a good starting point which helps with convergence
+    print('finding starting values... \n')
+    m.find_starting_values() # Find good starting values for optimization
+                             # uses gradient ascent optimization
+                             # finds the minimum of a function
+    print('\n')
+    print('starting values found... \n')
+    # starting drawing 7000 samples and discardin 5000 as burn-in
+    m.sample(2000, burn=20) # posterior samples
+
+    print('generating stats... \n')
+    stats = m.gen_stats()
+    stats[stats.index.isin(['a', 'a_std', 'a_subj.0', 'a_subj.1'])]
+
+    print('printing stats... \n')
+    m.print_stats()
+
+    print('\n')
+    
+    m.plot_posteriors(['a', 't'])
+
+
+    m.plot_posterior_predictive(figsize=(14, 10))
+    print('Legend: Red = Indiv Subject; Blue = Prediction')
+    # shows how well the model fits the data
+    # red is subj
+
+    time.sleep(5)
+
+    models = []
+    
+    for i in range(5):
+        m = hddm.HDDM(data)
+        m.find_starting_values()
+        m.sample(5000, burn=20)
+        models.append(m)
+
+    hddm.analyze.gelman_rubin(models)
+
+    m.plot_posterior_predictive(figsize=(14, 10))
+if __name__ == '__main__':
+    main()
+
+# mailing list-- but when using whole data set get several error messages
+# figure out
+
+# stim -- easy, medium, hard
+# 1 & 4
+# 2 & 5
+# 3 & 6
+# separation in spatial freq
+# correct -- response
+
+# figure out about pandas structure
