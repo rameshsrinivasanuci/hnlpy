6f418e5e03ddb2970361a222eecabff99f0f1787
mtisby <mariel@rchair.ss.uci.edu>
2019-08-08 15:08:49 -0700

    updates with filter function

--- mariel/PyMatCompare.py
+++ mariel/PyMatCompare.py
@@ -40,4 +40,35 @@ X = cond_list
 ucl = .5+L*s*(np.sqrt(l/(2-l)*(np.power((1-l), np.multiply(2, (range(len(cond_list))))))))
 lcl = .5-L*s*(np.sqrt(l/(2-l)*(np.power((1-l), np.multiply(2, (range(len(cond_list))))))))
 
-z = lfilter(l, [1, (1-l)], cond, zi=((1-l)/2))
+
+'''
+Error Checking Filter Function ~ Not Working
+x1 = np.array([l])
+x2 = np.array([1, (1-l)])
+x3 = np.array([(1-l)/2])
+
+print(type(x1), 'x1', type(x2), 'x2', type(x3), 'x3')
+
+z = lfilter(np.array([l]), np.array([1, (1-l)]), cond, zi= np.array([(1-l)/2]))
+'''
+
+# variables defined for filtering
+
+b = np.array([l])
+a = np.array([1, (l-1)])
+x = cond
+zi = (1-l)/2
+
+z = []
+for n in range(0, len(cond)):
+	print('n: ', n)
+	if n == 0:
+		z_add = (b[0]) * x[n][0] - (a[1]) * zi
+		z_add = a[0]*z_add
+		
+	else:
+		z_add = (b[0]) * x[n][0] - (a[1]) * z[n-1]
+		z_add = a[0]*z_add
+		
+
+	z.append(round(z_add, 4)) 
