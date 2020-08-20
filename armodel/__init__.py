b8eb2c768d44beb6396182d33058f66f90b51bc2
ramesh <r.srinivasan@uci.edu>
2020-08-20 16:07:03 -0700

    added armodel

---
+++ armodel/__init__.py
@@ -0,0 +1,6 @@
+from .pdc_dtf import mvar_generate
+from .pdc_dtf import cov
+from .pdc_dtf import mvar_fit
+from .pdc_dtf import compute_order
+from .pdc_dtf import spectral_density
+__all__ = ['mvar_generate','cov','mvar_fit','compute_order','spectral_density']
