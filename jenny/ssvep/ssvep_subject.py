62b59c7d740f96ef4c0063f7a5288da18f1b4a8a
jennyqsun <sunqinhua0726@gmail.com>
2020-11-20 15:58:12 -0800

    updated the version where fourier coefficient contains zero

--- jenny/ssvep/ssvep_subject.py
+++ jenny/ssvep/ssvep_subject.py
@@ -6,7 +6,7 @@ Created on Sun May 10 16:05:45 2020
 @author: jenny
 """
 
-# this script gets the subject
+# this scripts calculate the ssvep power for each subject
 
 # %%
 # import python modules
@@ -24,31 +24,43 @@ from scipy import signal
 import timeop
 #%%
 
-path = '/home/jenny/pdmattention/task3/'
+path = '/home/ramesh/pdmattention/task3/'
 # picking the training set
 subIDs = choose_subs(1, path)
 
 
+subID = 's231_ses2_'  # the subject I was using
+stimulus_ssvep, noise_ssvep, photocell = SSVEP_task3(subID)
+stim_power = np.abs(stimulus_ssvep['erp_fft'][30,:]) **2
+noise_power = 1/2 * (np.abs(stimulus_ssvep['erp_fft'][29,:]) **2 + np.abs(stimulus_ssvep['erp_fft'][31,:]) **2)
+snr = 2 * stim_power / noise_power
+
+
 def getSSVEP(data,sr,window,ssvep_freq,goodtrials,goodchans):
-    #create empty dictionary for output
+    """ this function has ssvep structure including fourier coefficients of erp and svd procedure """
     SSVEP = dict();
+
     #some renaming
     startsamp = window[0]
     endsamp = window[1]
     epochlength = data.shape[0]
     nchan = data.shape[1]
     ntrial = data.shape[2]
+
     # average erp.
     erp = np.mean(data[:, :, goodtrials], axis=2)    
+
     # FFT the ERPs
     # remove the mean
     erpmean = np.tile(np.mean(erp, axis=0), [epochlength, 1])
     erp = erp - erpmean
-    #take fft over prescribed window    
+
+    #take fft over prescribed window
     erpf = fft(erp[startsamp:endsamp, :], axis=0)/(endsamp-startsamp) # raw fft     
     binwidth = int((endsamp-startsamp)/sr)
-    u,s,vh = linalg.svd(erpf[(ssvep_freq-1)*binwidth:(ssvep_freq+1)*binwidth+1,goodchans])
-    snr = 2*np.abs(u[1,:])/(np.abs(u[0,:]+u[2,:]))
+    u,s,vh = linalg.svd(erpf[(ssvep_freq-1)*binwidth:(ssvep_freq+1)*binwidth+1,:])
+    snr = 2 * (np.abs(u[1,:]**2))/(np.abs(u[0,:])**2 + np.abs(u[2,:])**2)
+
     snrflagsignal = 1
     if np.max(snr) < 1:
         print('Warning NO SSVEP detected at stimulus frequency')
@@ -57,11 +69,15 @@ def getSSVEP(data,sr,window,ssvep_freq,goodtrials,goodchans):
     weights = np.zeros((nchan,1),dtype=complex)
 
 	# This is an optimal set of weights to estimate 30 hz signal. 
-    weights[goodchans,0] = np.matrix.transpose(vh[0,:])
+    weights[:,0] = np.matrix.transpose(vh[0,:])
 
 	# lets test it on the same interval using weighted electrode vs. original
     erpproject = np.matmul(erpf, weights)
-     
+
+    # multiply the weights for all timepoints
+    weights_long = np.tile(weights * np.diag(s)[0,0], [1,1000])
+    channel_power = np.transpose(erpf) * weights_long
+
     # Now use the weights to loop through indivial trials 
     trialestimate = np.zeros((endsamp-startsamp,ntrial),dtype=complex)
 
@@ -84,6 +100,8 @@ def getSSVEP(data,sr,window,ssvep_freq,goodtrials,goodchans):
     SSVEP['projectspectrum'] = erpproject
     SSVEP['singletrial'] = trialestimate
     SSVEP['weights'] = weights
+    SSVEP['power_sub'] = channel_power
+    SSVEP['singular'] = np.diag(s)
     return SSVEP
     
 
@@ -95,12 +113,14 @@ def SSVEP_task3(subID):
     pcdict = read_mat(path + subID + 'task3_photocells.mat')
     datadict = read_mat(path + subID + 'task3_final.mat')
     behavdict = read_mat(path + subID[0:5] + 'behavior_final.mat')
-    rt = behavdict['rt']
 
-
+    data = np.array(datadict['data'])
     artifact = np.array(datadict['artifact'])
     sr = np.array(datadict['sr'])
     beh_ind = np.array(behavdict['trials'])
+    condition = np.array(behavdict['condition'])
+    rt = behavdict['rt']
+    correct = behavdict['correct']
 
     # open up indices
     artifact0 = artifact.sum(axis=0)
@@ -110,33 +130,34 @@ def SSVEP_task3(subID):
     goodtrials = np.squeeze(np.array(np.where(artifact0 < 20)))
     goodchans = np.squeeze(np.array(np.where(artifact1 < 40)))
 
-    # choosing the trials that have RT over 300ms from the goodtrial list
-    finalgoodtrials = np.array(compLists(beh_ind, goodtrials))
-    rt_finalgoodtrials = behavdict['rt']
+    # choosing the trials that have RT over 300ms and check if they are all goodtrials
+    # get the index and apply to rt, condition and accuracy
+    ind, finalgoodtrials = np.array(compListsInd(beh_ind, goodtrials))
+    rt_final = rt[ind]
+    condition_final = condition[ind]
+    correct_final = correct[ind]
 
-
-
+    # get the photocells
+    photocell = dict()
     p = pcdict['photostim']
     n = pcdict['photonoise']
-    photocell_ssvep = dict()
-    # FFT the photocells stimulus
-    
-
-    window = [1250,2250]
-    
-    stimulus_ssvep = getSSVEP(data,sr,window,30,finalgoodtrials,goodchans)
-    noise_ssvep = getSSVEP(data,sr,window,40,finalgoodtrials,goodchans)
-    
     yfp = fft(p[1250:2250,:], axis=0)
     yfn = fft(n[1250:2250,:], axis=0)
-    photocell_ssvep['stim'] = p
-    photocell_ssvep['noise'] = n
-    SSVEP['stim_fft'] = yfp
-    SSVEP['noise_fft'] = yfn
+    photocell['stim'] = p
+    photocell['noise'] = n
+    photocell['stim_fft'] = yfp
+    photocell['noise_fft'] = yfn
 
-    
-    return stimulus_ssvep, noise_ssvep, photocell_ssvep
+    # time window of interest
+    window = [1250, 2250]
+
+    # FFT the eeg data
+    stimulus_ssvep = getSSVEP(data,sr,window,30,finalgoodtrials,goodchans)
+    noise_ssvep = getSSVEP(data,sr,window,40,finalgoodtrials,goodchans)
+
+    return stimulus_ssvep, noise_ssvep, photocell
 #%%
+
 
 
 def choose_subs(lvlAnalysis, path):
@@ -173,233 +194,193 @@ def choose_subs(lvlAnalysis, path):
     return subIDs
 
 
-def compLists(list1, list2):
-    final = [element for element in list1 if element in list2]
-
-    return final
+def compListsInd(list1, list2):
+    final = [element for ind, element in enumerate(list1) if element in list2]
+    ind = [ind for ind, element in enumerate(list1) if element in list2]
+    return ind, final
 
 #
-#for subID in subIDs2:
-#    ssvep_stimulus,ssvep_noise, ssvep_photocell = SSVEP_task3(subID)
-#    outname = path+subID+'_stimulus_SSVEP.mat'
-#    savemat(outname,)            
-
-#%%
-
-
-
-
-
-    #    This is just trying to  fft each channel
-    #    trialestimate = np.zeros((4000,129,360))
-    #    for trial in finalgoodtrials: 
-    #        trialdata = np.squeeze(data[:,:,trial])
-    #        trialdata = trialdata * np.transpose(weightsn)
-    #        trialestimate[:,:,trial] = trialdata[:,:]
-    #    
-    #    # find the onset of noise for each trial
-    #    noise = pcdict['photonoise']
-    #    noisetrialestimate = np.zeros((1000,129,360))
-    #    for i in finalgoodtrials:
-    #        firstind = np.argmax(noise[:, i] > 0)
-    #        lastind = firstind + 1000
-    #        noisetrialestimate[:,:,i] = trialestimate[firstind:lastind,:,i]
-        
-    #
-    #        # FFT the photocells
-    #    plt.subplot(322)
-    #    for x in range(0,numtrials):
-    #        y = p[1250:2249,x]
-    #        N = len(y)
-    #        yf = fft(y)
-    #        xf = np.linspace(0.0, sr/2, N//2)
-    #        plt.plot(xf[0:100], 2/N * np.abs(yf[0:100]))
-    #    plt.grid()
-    #    plt.title('Photocell Stimulus')
-    #
-    #        # FFT the noise
-    #    noisesignals = []
-    #    plt.subplot(324)
-    #    for x in range(0,numtrials):
-    #        firstind = np.argmax(n[:,x]>0)
-    #        lastind = firstind + 1500
-    #        noisesignal = n[firstind:lastind,x]
-    #        N = len(noisesignal)
-    #        yn = fft(noisesignal)
-    #        xn = np.linspace(0.0, sr/2, N//2)
-    #        plt.plot(xn[0:100], 2/N * np.abs(yn[0:100]))
-    #    plt.grid()
-    #    plt.title('Photocell Noise')
-    #
-    #        # FFT the for erp
-    #    plt.subplot(326)
-    #    for x in range(0,129):
-    #        y = erp[1250:2249,x]
-    #        N = len(y)
-    #        yf = fft(y)
-    #        xf = np.linspace(0.0, sr/2, N//2)
-    #        plt.plot(xf[0:60], 2/N * np.abs(yf[0:60]))
-    #        plt.title('ERP')
-    #    plt.grid()
-
-
-# %%
-# globals
-SSVEP = getSSVEP(subID)
-
-#%%
-## %% Plots
+# #
+# #for subID in subIDs2:
+# #    ssvep_stimulus,ssvep_noise, ssvep_photocell = SSVEP_task3(subID)
+# #    outname = path+subID+'_stimulus_SSVEP.mat'
+# #    savemat(outname,)
 #
- #photocell stimulus 
-
- plt.figure()
- plt.subplot(411)
- npc = len(yfp)
- xfp = np.linspace(0.0, sr / 2, npc // 2)
- plt.plot(xfp[0:100], 2 / npc * np.abs(yfp[0:100, :]))
- plt.title('Photocell Stimulus using fft')
- plt.xticks(np.arange(min(xfp[0:100]), max(xfp[0:100]) + 1, 10))
-
- #photocell noise
-
- plt.subplot(412)
- nn = len(noisesignal)
- xn = np.linspace(0.0, sr / 2, nn // 2)
- plt.plot(xn[0:100], 2 / nn * np.abs(yfn[0:100]))
- plt.title('Photocell Noise fft')
- plt.xticks(np.arange(min(xn[0:100]), max(xn[0:100]) + 1, 10))
-
-    
- #ERP after stim
- plt.subplot(413)
- nerp = len(yferp)
- xf = np.linspace(0.0, sr / 2, (nerp // 2 + 1))
- xfplot = xf[8*int(len(xf)/nyquist):51*int(len(xf)/nyquist)]
- yfplot = yferp[8*int(len(xf)/nyquist):51*int(len(xf)/nyquist)]
- plt.plot(xfplot, (2 * np.abs(yfplot)) ** 2)
- plt.title('ERP (matched with onset of stimulus) for %i ms' % nerp)
- plt.xticks(np.arange(min(xf[0:60]), max(xf[0:60]) + 1, 10))    
-
- #ERP after noise
-plt.subplot(414)
-nerpn = len(erpnoise)  
-xferpn = np.linspace(0.0, sr / 2, nerpn // 2 +1)
-xferpnplot = xferpn[8*int(len(xferpn)/nyquist):51*int(len(xferpn)/nyquist)]
-yferpnplot = yferpn[8*int(len(xferpn)/nyquist):51*int(len(xferpn)/nyquist)]
-plt.plot(xferpnplot, (2 * np.abs(yferpnplot)) ** 2)
-plt.title('ERP fft(matched with onset of noise) for %i ms' % erpdur)
-plt.xticks(np.arange(min(xferpn[0:60]), max(xferpn[0:60]) + 1, 10))
-                            
-plt.tight_layout()
-plt.show
+# #%%
 #
-#    # optimal channel to detect noise (40Hz)
-#    # 35Hz-45Hz bin (1000ms)    
-#    ferpn = fft(erpnoise, axis=0) # raw fft
-#    binwidth = int((len(erpnoise)/2 + 1)/nyquist)
-#    ferpn = ferpn[35*binwidth:45*binwidth +1]
-#    u,s,vh = linalg.svd(ferpn[:,goodchan])
-#    
-#    weightsn = np.zeros((129,1),dtype=complex)
 #
-#	# This is an optimal set of weights to estimate 35-45hz signal. 
-#    weightsn[goodchan,0] = np.matrix.transpose(vh[0,:])
 #
-#	# lets test it on the same interval using weighted electrode vs. original
-#    plt.figure()
-#    plt.subplot(311)
-#    yftest = fft(erpnoise, axis=0) 
-#    erpnoisetest = np.matmul(yftest,weightsn)
-#    xferpn = np.linspace(0.0, nyquist, nerpn // 2 +1)
-#    plt.plot(xferpn[8:51], (2/len(erpnoise) * np.abs(erpnoisetest[8:51]) ** 2))
-#    plt.title('Testing the weighted channels on the ERP post-noise')
-#    
-#        
-#    plt.subplot(312)
-#    yerp = fft(erpnoise, axis=0) 
-#    xferpn = np.linspace(0.0, nyquist, nerpn // 2 +1)
-#    plt.plot(xferpn[8:51], (2/len(erpnoise) * np.abs(yerp[8:51]) ** 2))
-#    plt.title('Original unweighted ERP')
-# 
-#    Testing   
-#   plt.figure()
-#    plt.subplot(311)
-#    xf = np.linspace(0.0, nyquist, nerp // 2 +1)
-#    plt.plot(xf[8:51], (2/len(erptest) * np.abs(erptest[8:51]) ** 2))
-#    plt.title('Testing the weighted channels on the ERP post-stimulus')
-#    
-#        
-#    plt.subplot(312)
-#    yferp = fft(y, axis=0) 
-#    plt.plot(xf[8:51], (2/len(erptest) * np.abs(yferp[8:51]) ** 2))
-#    plt.title('Original Unweighted ERP')
 #
-#    
-#    # fft the trial estimate 1000ms after noise (25-35hz bins used)
-#    plt.subplot(313)
-#    plt.plot(xf[25:51], (2/1000 * np.abs(trialestimatestim[25:51,0:10]) ** 2))
-#    plt.title('trial estimate for stim signals after fft using 25Hz-35Hz')
-    
-   
-#    # Now use the weights to loop through indivial trials 
-#    noise = pcdict['photonoise']
-#    trialestimate = np.zeros((1000,360),dtype=complex)
-#    for trial in finalgoodtrials: 
-#        trialdata = np.squeeze(data[:,:,trial])
-#        firstind = np.argmax(n[:, trial] > 0)
-#        lastind = firstind + 1000
-#        trialdata = trialdata[firstind:lastind,:]
-#        trialfft = fft(trialdata, axis = 0)
-#        trialweighted = np.matmul(trialfft,weightsn)
-#        trialestimate[:,trial] = trialweighted[:,0] #new time series
-#    
-#    # fft the trial estimate 1000ms after noise (35-45hz bins)
-#    plt.subplot(313)
-#    xferpn = np.linspace(0.0, nyquist, nerp // 2 +1)
-#    plt.plot(xferpn[20:51], (2/1000 * np.abs(trialestimate[20:51,:]) ** 2))
-#    plt.title('trial estimate for noise signals after fft using 35Hz-45Hz')
-#    
-#    plt.tight_layout()
-#    plt.show
+#
+#     #    This is just trying to  fft each channel
+#     #    trialestimate = np.zeros((4000,129,360))
+#     #    for trial in finalgoodtrials:
+#     #        trialdata = np.squeeze(data[:,:,trial])
+#     #        trialdata = trialdata * np.transpose(weightsn)
+#     #        trialestimate[:,:,trial] = trialdata[:,:]
+#     #
+#     #    # find the onset of noise for each trial
+#     #    noise = pcdict['photonoise']
+#     #    noisetrialestimate = np.zeros((1000,129,360))
+#     #    for i in finalgoodtrials:
+#     #        firstind = np.argmax(noise[:, i] > 0)
+#     #        lastind = firstind + 1000
+#     #        noisetrialestimate[:,:,i] = trialestimate[firstind:lastind,:,i]
+#
+#     #
+#     #        # FFT the photocells
+#     #    plt.subplot(322)
+#     #    for x in range(0,numtrials):
+#     #        y = p[1250:2249,x]
+#     #        N = len(y)
+#     #        yf = fft(y)
+#     #        xf = np.linspace(0.0, sr/2, N//2)
+#     #        plt.plot(xf[0:100], 2/N * np.abs(yf[0:100]))
+#     #    plt.grid()
+#     #    plt.title('Photocell Stimulus')
+#     #
+#     #        # FFT the noise
+#     #    noisesignals = []
+#     #    plt.subplot(324)
+#     #    for x in range(0,numtrials):
+#     #        firstind = np.argmax(n[:,x]>0)
+#     #        lastind = firstind + 1500
+#     #        noisesignal = n[firstind:lastind,x]
+#     #        N = len(noisesignal)
+#     #        yn = fft(noisesignal)
+#     #        xn = np.linspace(0.0, sr/2, N//2)
+#     #        plt.plot(xn[0:100], 2/N * np.abs(yn[0:100]))
+#     #    plt.grid()
+#     #    plt.title('Photocell Noise')
+#     #
+#     #        # FFT the for erp
+#     #    plt.subplot(326)
+#     #    for x in range(0,129):
+#     #        y = erp[1250:2249,x]
+#     #        N = len(y)
+#     #        yf = fft(y)
+#     #        xf = np.linspace(0.0, sr/2, N//2)
+#     #        plt.plot(xf[0:60], 2/N * np.abs(yf[0:60]))
+#     #        plt.title('ERP')
+#     #    plt.grid()
+#
+#
+# # %%
+# # globals
+# SSVEP = getSSVEP(subID)
+#
+# #%%
+# ## %% Plots
+# #
+#  #photocell stimulus
+#
+#  plt.figure()
+#  plt.subplot(411)
+#  npc = len(yfp)
+#  xfp = np.linspace(0.0, sr / 2, npc // 2)
+#  plt.plot(xfp[0:100], 2 / npc * np.abs(yfp[0:100, :]))
+#  plt.title('Photocell Stimulus using fft')
+#  plt.xticks(np.arange(min(xfp[0:100]), max(xfp[0:100]) + 1, 10))
+#
+#  #photocell noise
+#
+#  plt.subplot(412)
+#  nn = len(noisesignal)
+#  plt.xticks(np.arange(min(xn[0:100]), max(xn[0:100]) + 1, 10))
+#
+#
+#  #ERP after stim
+#  plt.subplot(413)
+#  nerp = len(yferp)
+#  xf = np.linspace(0.0, sr / 2, (nerp // 2 + 1))
+#  xfplot = xf[8*int(len(xf)/nyquist):51*int(len(xf)/nyquist)]
+#  yfplot = yferp[8*int(len(xf)/nyquist):51*int(len(xf)/nyquist)]
+#  plt.plot(xfplot, (2 * np.abs(yfplot)) ** 2)
+#  plt.title('ERP (matched with onset of stimulus) for %i ms' % nerp)
+#  plt.xticks(np.arange(min(xf[0:60]), max(xf[0:60]) + 1, 10))
+#
+#  #ERP after noise
+# plt.subplot(414)
+# nerpn = len(erpnoise)
+# xferpn = np.linspace(0.0, sr / 2, nerpn // 2 +1)
+# xferpnplot = xferpn[8*int(len(xferpn)/nyquist):51*int(len(xferpn)/nyquist)]
+# yferpnplot = yferpn[8*int(len(xferpn)/nyquist):51*int(len(xferpn)/nyquist)]
+# plt.plot(xferpnplot, (2 * np.abs(yferpnplot)) ** 2)
+# plt.title('ERP fft(matched with onset of noise) for %i ms' % erpdur)
+# plt.xticks(np.arange(min(xferpn[0:60]), max(xferpn[0:60]) + 1, 10))
+#
+# plt.tight_layout()
+# plt.show
+# #
+# #    # optimal channel to detect noise (40Hz)
+# #    # 35Hz-45Hz bin (1000ms)
+# #    ferpn = fft(erpnoise, axis=0) # raw fft
+# #    binwidth = int((len(erpnoise)/2 + 1)/nyquist)
+# #    ferpn = ferpn[35*binwidth:45*binwidth +1]
+# #    u,s,vh = linalg.svd(ferpn[:,goodchan])
+# #
+# #    weightsn = np.zeros((129,1),dtype=complex)
+# #
+# #	# This is an optimal set of weights to estimate 35-45hz signal.
+# #    weightsn[goodchan,0] = np.matrix.transpose(vh[0,:])
+# #
+# #	# lets test it on the same interval using weighted electrode vs. original
+# #    plt.figure()
+# #    plt.subplot(311)
+# #    yftest = fft(erpnoise, axis=0)
+# #    erpnoisetest = np.matmul(yftest,weightsn)
+# #    xferpn = np.linspace(0.0, nyquist, nerpn // 2 +1)
+# #    plt.plot(xferpn[8:51], (2/len(erpnoise) * np.abs(erpnoisetest[8:51]) ** 2))
+# #    plt.title('Testing the weighted channels on the ERP post-noise')
+# #
+# #
+# #    plt.subplot(312)
+# #    yerp = fft(erpnoise, axis=0)
+# #    xferpn = np.linspace(0.0, nyquist, nerpn // 2 +1)
+# #    plt.plot(xferpn[8:51], (2/len(erpnoise) * np.abs(yerp[8:51]) ** 2))
+# #    plt.title('Original unweighted ERP')
+# #
+# #    Testing
+# #   plt.figure()
+# #    plt.subplot(311)
+# #    xf = np.linspace(0.0, nyquist, nerp // 2 +1)
+# #    plt.plot(xf[8:51], (2/len(erptest) * np.abs(erptest[8:51]) ** 2))
+# #    plt.title('Testing the weighted channels on the ERP post-stimulus')
+# #
+# #
+# #    plt.subplot(312)
+# #    yferp = fft(y, axis=0)
+# #    plt.plot(xf[8:51], (2/len(erptest) * np.abs(yferp[8:51]) ** 2))
+# #    plt.title('Original Unweighted ERP')
+# #
+# #
+# #    # fft the trial estimate 1000ms after noise (25-35hz bins used)
+# #    plt.subplot(313)
+# #    plt.plot(xf[25:51], (2/1000 * np.abs(trialestimatestim[25:51,0:10]) ** 2))
+# #    plt.title('trial estimate for stim signals after fft using 25Hz-35Hz')
+#
+#
+# #    # Now use the weights to loop through indivial trials
+# #    noise = pcdict['photonoise']
+# #    trialestimate = np.zeros((1000,360),dtype=complex)
+# #    for trial in finalgoodtrials:
+# #        trialdata = np.squeeze(data[:,:,trial])
+# #        firstind = np.argmax(n[:, trial] > 0)
+# #        lastind = firstind + 1000
+# #        trialdata = trialdata[firstind:lastind,:]
+# #        trialfft = fft(trialdata, axis = 0)
+# #        trialweighted = np.matmul(trialfft,weightsn)
+# #        trialestimate[:,trial] = trialweighted[:,0] #new time series
+# #
+# #    # fft the trial estimate 1000ms after noise (35-45hz bins)
+# #    plt.subplot(313)
+# #    xferpn = np.linspace(0.0, nyquist, nerp // 2 +1)
+# #    plt.plot(xferpn[20:51], (2/1000 * np.abs(trialestimate[20:51,:]) ** 2))
+# #    plt.title('trial estimate for noise signals after fft using 35Hz-45Hz')
+# #
+# #    plt.tight_layout()
+# #    plt.show
+#
+#
+# # these are the scripts from diffusion.py
 
-
-# these are the scripts from diffusion.py
-
-def choose_subs(lvlAnalysis, path):
-    # this function returns a list of subject IDs and task based on whether or not this is testing
-    # analysis or analysis of all subjects
-    # --------------------------------------------------------------------------
-    # returns a list of subject IDs
-
-    allDataFiles = os.listdir(path)
-
-    if lvlAnalysis == 1:
-        excludeSubs = ['s184', 's187', 's190', 's193', 's199', 's209', 's214', 's220', 's225', 's228', \
-                       's234', 's240', 's213', 's235']
-
-        subIDs = []
-
-        for files in allDataFiles:
-            if (files[16:] == 'expinfo.mat') and ((files[:4] not in excludeSubs) == True):
-                subIDs.append(files[0:10])
-
-    elif lvlAnalysis == 2:
-        subIDs = []
-        for files in allDataFiles:
-            if files[16:] == 'expinfo.mat':
-                subIDs.append(files[0:10])
-
-    try:
-        subIDs.remove('s193_ses2_')
-        subIDs.remove('s193_ses1_')
-
-    except:
-        pass
-
-    return subIDs
-
-
-def compLists(list1, list2):
-    final = [element for element in list1 if element in list2]
-
-    return final
